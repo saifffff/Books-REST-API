56d2f2b39f9cf96904fbef75c9d6c0d5bace2d7a
saifffff <angryn1nj4@gmail.com>
2024-03-26 18:05:49 +0530

    Changed handlers to send appropriate error codes using ResponseEntity

--- src/main/java/com/restapi/app/controllers/FrontController.java
+++ src/main/java/com/restapi/app/controllers/FrontController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,31 +23,44 @@ public class FrontController {
 	@Autowired
 	private BookRepository bookDao;
 	
-	@GetMapping("/books")
-	public List<Book> getAllBooks() {
+	@GetMapping("/book/all")
+	public ResponseEntity<List<Book>> getAllBooks() {
 		ArrayList<Book> allBooks = (ArrayList<Book>) this.bookDao.findAll();
-		return allBooks;
+		if(allBooks.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.of(Optional.of(allBooks));
 	}
 	
 	@GetMapping("/book/{id}")
-	public Optional<Book> getBook(@PathVariable("id")int id) {
+	public ResponseEntity<Optional<Book>> getBook(@PathVariable("id")int id) {
 		Optional<Book> mybook = null;
 		mybook = this.bookDao.findById(id);
-		return mybook;
+		if(!mybook.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.of(Optional.of(mybook));
 	}
 	
-	@GetMapping("/book/author/{writer}")
-	public List<Book> getBookByWriter(@PathVariable("writer")String writer) {
+	@GetMapping("/book/all/{writer}")
+	public ResponseEntity<List<Book>> getBookByWriter(@PathVariable("writer")String writer) {
 		ArrayList<Book> allBooksByWriter = null;
 		allBooksByWriter = (ArrayList<Book>) this.bookDao.findByWriter(writer);
-		return allBooksByWriter;
+		if(allBooksByWriter.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.of(Optional.of(allBooksByWriter));
 	}
 	
 	
 	@PostMapping("/add/book")
-	public Book createBook(@RequestBody Book book) {
+	public ResponseEntity<Book> createBook(@RequestBody Book book) {
+		Optional<Book> chkbk = this.bookDao.findById(book.getId());
+		if(chkbk.isPresent()) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 		Book rv = this.bookDao.save(book);
-		return rv;
+		return ResponseEntity.of(Optional.of(rv));
 	}
 	
 	 @PostMapping("/add/books")
